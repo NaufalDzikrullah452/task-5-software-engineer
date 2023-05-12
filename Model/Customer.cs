22a1fe2c30b2dc34d013c29ac7525f73019989fd
NaufalDzikrullah452 <naufal.dzikrullah452@gmail.com>
2023-05-12 21:05:46 +0700

    EF module

---
+++ Model/Customer.cs
@@ -0,0 +1,39 @@
+﻿namespace CustomerApi.Model {
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using Microsoft.EntityFrameworkCore;
+  
+   
+public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string HomeAddress { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("<connection-string>");
+        }
+    }
+}
